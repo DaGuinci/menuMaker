643459e464226a387a3a035dd77d2de2c8068e8b
DaGuinci <daguinci@mailo.com>
2024-10-22 11:50:36 +0200

    feat: get and post ingredient without env viariables

---
+++ src/main/java/com/daguinci/menumaker_api/Ingredient.java
@@ -0,0 +1,61 @@
+package com.daguinci.menumaker_api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String type;
+
+    private Boolean seasonal;
+
+    private Integer[] seasons;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getSeasonal() {
+        return seasonal;
+    }
+
+    public void setSeasonal(Boolean seasonal) {
+        this.seasonal = seasonal;
+    }
+
+    public Integer[] getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(Integer[] seasons) {
+        this.seasons = seasons;
+    }
+}
---
+++ src/main/java/com/daguinci/menumaker_api/IngredientRepository.java
@@ -0,0 +1,12 @@
+package com.daguinci.menumaker_api;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.daguinci.menumaker_api.Ingredient;;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
+
+}
