cddd92573108a06416b5b45ef7902e4f769548f2
DaGuinci <daguinci@mailo.com>
2024-10-22 09:39:12 +0200

    core: initialize spring

---
+++ src/main/java/com/daguinci/menumaker_api/MenumakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.daguinci.menumaker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenumakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenumakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daguinci/menumaker_api/MenumakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daguinci.menumaker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenumakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
